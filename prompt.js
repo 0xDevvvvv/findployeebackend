eeb74069328f7f78da80953329e529275be9736c
Dev SK <devbhagavan25@gmail.com>
2024-11-03 02:43:19 +0530

    prompt modification

--- prompt.js
+++ prompt.js
@@ -6,9 +6,12 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function getPrompt(prompt) {
-    const finalprompt = '"Employee 1: {    skills: ["Python", "Machine Learning", "Data Analysis"]}Employee 2: {    skills: ["JavaScript", "React", "Frontend Development"]}Employee 3: {    skills: ["SQL", "Data Engineering", "Cloud Computing"]}"' 
-    + "Give only the employee numbers with no additional text for a task that requires" 
-    + prompt;
+    skills = "artificial intelligence,data,development tools,end user applications,infrastructure and cloud,media,operating systems,programming languages,science and medicine,security,social and communication,web"
+    const finalprompt = "Given these skills"
+    + skills
+    + "Give only the relevant skills from this list for the task given ." 
+    + prompt
+    +"No other output is required, no other formatting is also required only comma seperated";
     const data = await model.generateContent(finalprompt);
     return data.response.text();
 }
