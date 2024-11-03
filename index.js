736350aeb21723ce262696bb1e2357f29bdd646f
Dev SK <devbhagavan25@gmail.com>
2024-11-03 10:08:16 +0530

    many things addedd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPrompt} = require('./prompt')
+const {getSkillsPrompt,getEmployeePrompt} = require('./prompt')
 const cors = require('cors');
 // Create an instance of Express
 const app = express();
@@ -14,7 +14,21 @@ app.post('/', (req, res) => {
   try {
     // Convert JSON body to a string
     const values = Object.values(req.body);
-    const result = getPrompt(values).then((response) => res.send(`${response}`));
+    const result = getSkillsPrompt(values).then((response) => res.send(`${response}`));
+
+    // Send the values array as a response
+  } catch (error) {
+    // Handle any potential errors
+    res.status(400).send('Invalid JSON');
+  }
+});
+
+app.post('/getEmpID', (req, res) => {
+  try {
+    // Convert JSON body to a string
+    const values = Object.values(req.body);
+    console.log(values[0])
+    const result = getEmployeePrompt(values[0],values[1]).then((response) => res.send(`${response}`));
 
     // Send the values array as a response
   } catch (error) {
--- prompt.js
+++ prompt.js
@@ -5,15 +5,24 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-async function getPrompt(prompt) {
+async function getSkillsPrompt(prompt) {
     skills = "artificial intelligence,data,development tools,end user applications,infrastructure and cloud,media,operating systems,programming languages,science and medicine,security,social and communication,web"
     const finalprompt = "Given these skills"
     + skills
-    + "Give only the relevant skills from this list for the task given ." 
+    + "Give only the 3 relevant and necessary skills from this list for the task given." 
     + prompt
-    +"No other output is required, no other formatting is also required only comma seperated";
+    +"No other output is required, no other formatting is required, give the result back as comma seperated values.";
     const data = await model.generateContent(finalprompt);
     return data.response.text();
 }
-
-module.exports = {getPrompt};
+async function getEmployeePrompt(employee,skills) {
+    console.log(employee,skills)
+    const finalprompt = "Given the following employee details, the empid and selectedskills, "
+    +employee
+    +"return the empid of the employees who closely matches the following skills"
+    +skills
+    +"No other output is required , no other formatting is required , only the empid as comma seperated values"
+    const data = await model.generateContent(finalprompt);
+    return data.response.text();
+}
+module.exports = {getSkillsPrompt,getEmployeePrompt};
